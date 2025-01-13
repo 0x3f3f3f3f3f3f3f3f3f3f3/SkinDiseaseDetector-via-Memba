74e0e7da1ad05990387dfedeb8910a3fed189abe
mac <0x3f3f3f3f3f3f@gmail.com>
2025-01-13 16:25:08 +0800

    SkinDiseaseDetector

--- photoProcessor.py
+++ photoProcessor.py
@@ -9,7 +9,6 @@ def is_valid_image(path):
     except (OSError, IOError):
         return False
 
-# 遍历数据集目录，删除损坏的图像
 dataset_dir = "/root/autodl-tmp/dataset/skinDisease_split/test"
 for root, _, files in os.walk(dataset_dir):
     for file in files:
